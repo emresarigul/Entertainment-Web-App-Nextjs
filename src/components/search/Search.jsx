ece86710fb78d90a6bd095d1219cc32a8e468e9a
emresarigul <emresarigul91@gmail.com>
2024-04-02 03:02:06 +0300

    shadcn ui

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -3,12 +3,19 @@ import React, { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { useRouter } from "next/navigation";
 import { IoSettingsOutline } from "react-icons/io5";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Search() {
   const router = useRouter();
   const [showName, setShowName] = useState("");
   const [showType, setShowType] = useState({
-    text: "Searching in movies",
+    text: "Search in movies",
     showType: "movie",
     barStatue: false,
   });
@@ -44,7 +51,7 @@ export default function Search() {
           value={showName}
           placeholder="Search..."
         />
-        <div className="absolute top-2 md:top-2.5 right-4 md:right-11 text-right text-sm text-white/50 w-40">
+        <div className="absolute -top-1 md:top-0.5 right-0 md:right-11 text-right text-sm w-10 md:w-40">
           <div
             onClick={() => {
               setShowType({
@@ -54,10 +61,34 @@ export default function Search() {
             }}
             className="cursor-pointer relative"
           >
-            <div className="mr-6 hidden lg:block">{`Searching in ${showType.showType}s`}</div>
-            <IoSettingsOutline className="absolute right-0 top-[2px] md:text-base" />
+            {/* <div className="mr-6 hidden lg:block">{`Searching in ${showType.showType}s`}</div>
+            <IoSettingsOutline className="absolute right-0 top-[2px] md:text-base" /> */}
+            <Select
+              onValueChange={(showType) => {
+                setShowType({
+                  barStatue: false,
+                  showType: showType,
+                  text: showType,
+                });
+              }}
+              defaultValue="movie"
+            >
+              <SelectTrigger className="md:w-[180px] bg-transparent border-none text-white/50 focus:ring-0 focus:ring-offset-0">
+                <SelectValue placeholder={showType.text} />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="movie">Search in movies</SelectItem>
+                <SelectItem value="serie">Search in series</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
-          <select
+        </div>
+      </form>
+    </div>
+  );
+}
+
+/*<select
             onChange={(e) => {
               setShowType({
                 barStatue: false,
@@ -91,9 +122,4 @@ export default function Search() {
             >
               Search in series
             </option>
-          </select>
-        </div>
-      </form>
-    </div>
-  );
-}
+          </select>*/
